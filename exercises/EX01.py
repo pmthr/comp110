4b3349abec6fcce6d6fb9361ccf21a9310f8c7b8
Pooja Mathur <pmathur@unc.edu>
2022-07-29 13:30:27 -0400

    EX01 - Input and Variables

---
+++ exercises/EX01.py
@@ -0,0 +1,9 @@
+# Practice with concatenation and numeric operators
+
+left_side: int = int(input("Left-hand side: "))
+right_side: int = int(input("Right-hand side: "))
+
+print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side ** right_side))
+print(str(left_side) + " / " + str(right_side) + " is " + str(left_side / right_side))
+print(str(left_side) + " // " + str(right_side) + " is " + str(left_side // right_side))
+print(str(left_side) + " % " + str(right_side) + " is " + str(left_side % right_side))
