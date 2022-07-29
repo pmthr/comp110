94effd073065f595a0f516e8630da55b4be356e0
Pooja Mathur <pmathur@unc.edu>
2022-07-29 13:35:12 -0400

    Delete EX01.py

--- exercises/EX01.py
+++
@@ -1,9 +0,0 @@
-# Practice with concatenation and numeric operators
-
-left_side: int = int(input("Left-hand side: "))
-right_side: int = int(input("Right-hand side: "))
-
-print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side ** right_side))
-print(str(left_side) + " / " + str(right_side) + " is " + str(left_side / right_side))
-print(str(left_side) + " // " + str(right_side) + " is " + str(left_side // right_side))
-print(str(left_side) + " % " + str(right_side) + " is " + str(left_side % right_side))
